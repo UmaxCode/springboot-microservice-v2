c759675b33ab3b6f537ee7f1c8ee55c7471d67be
umaxcode <maxwellodoom1729@gmail.com>
2024-11-16 21:51:52 +0000

    update test case with wiremock

--- order-service/src/test/java/com/umaxcode/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/umaxcode/microservices/order/OrderServiceApplicationTests.java
@@ -1,15 +1,21 @@
 package com.umaxcode.microservices.order;
 
+import com.umaxcode.microservices.order.domain.dto.OrderRequestDTO;
+import com.umaxcode.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
+
+import java.math.BigDecimal;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
     @LocalServerPort
@@ -24,26 +30,26 @@ class OrderServiceApplicationTests {
     @Test
     void placeOrder_ShouldCreateOrder() {
 
-        String submitOrderRequest = """
-                {
-                    "skuCode": "iphone_15",
-                    "price": 1000,
-                    "quantity": 1
-                }
-                """;
+        OrderRequestDTO request = OrderRequestDTO.builder()
+                .skuCode("iphone_6")
+                .price(BigDecimal.valueOf(1000))
+                .quantity(1)
+                .build();
+
+        InventoryClientStub.stubInventoryCall(request.skuCode(), request.quantity());
+
         RestAssured.given()
                 .contentType("application/json")
-                .body(submitOrderRequest)
+                .body(request)
                 .when()
-                .post("/api/v1/orders")
+                .post("/orders")
                 .then()
                 .log().all()
                 .statusCode(201)
                 .body("id", Matchers.notNullValue())
-                .body("skuCode", Matchers.is("iphone_15"))
+                .body("skuCode", Matchers.is("iphone_6"))
                 .body("price", Matchers.greaterThanOrEqualTo(1000))
                 .body("quantity", Matchers.equalTo(1));
-
     }
 
 }
---
+++ order-service/src/test/java/com/umaxcode/microservices/order/stubs/InventoryClientStub.java
@@ -0,0 +1,14 @@
+package com.umaxcode.microservices.order.stubs;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class InventoryClientStub {
+
+    public static void stubInventoryCall(String skuCode, int quantity) {
+        stubFor(get(urlEqualTo("/api/v1/inventories/status?skuCode=" + skuCode + "&quantity=" + quantity))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("true")));
+    }
+}
